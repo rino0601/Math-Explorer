113ff9fa8160aeef7ea3b73caf07be755577940b
Hyun Hwang <hhwang@cyrusian.com>
2012-07-08 23:04:24 +0900

    MEAskViewController를 템플릿화시키고 Reading용 Ask Modal을 추가.

---
+++ Math Explorer/MEReadingAskViewController.h
@@ -0,0 +1,13 @@
+//
+//  MEReadingAskViewController.h
+//  Math Explorer
+//
+//  Created by Hyun Hwang on 7/8/12.
+//  Copyright (c) 2012 SI Cyrusian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEReadingAskViewController : UIViewController
+
+@end
---
+++ Math Explorer/MEReadingAskViewController.m
@@ -0,0 +1,44 @@
+//
+//  MEReadingAskViewController.m
+//  Math Explorer
+//
+//  Created by Hyun Hwang on 7/8/12.
+//  Copyright (c) 2012 SI Cyrusian. All rights reserved.
+//
+
+#import "MEReadingAskViewController.h"
+
+@interface MEReadingAskViewController ()
+
+@end
+
+@implementation MEReadingAskViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
